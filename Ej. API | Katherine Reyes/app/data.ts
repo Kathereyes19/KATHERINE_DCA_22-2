cbf6b2900cdbfb52b8c2efac26a50fbbdfb6d347
Katherine Reyes <98465512+Kathereyes19@users.noreply.github.com>
2022-10-02 21:51:22 -0500

    Delete Ej. API | Katherine Reyes directory

--- Ej. API | Katherine Reyes/app/data.ts
+++
@@ -1,9 +0,0 @@
-import {Attributes} from "./components/characters/characters.js"
-
-
-export const Data = async (): Promise<Array<Attributes>> => {
-      const response = await fetch("https://www.breakingbadapi.com/api//characters");
-      const data = await response.json();
-      console.log(data);
-      return data;
-};
--- Ej. API | Katherine Reyes/index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express")
-const server = express
-
-server.request(express.static(__dirname + "public"));
-server.listen(8080, () => console.log("servidor corriendo..."));
